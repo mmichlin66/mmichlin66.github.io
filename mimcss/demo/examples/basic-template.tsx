9072a745eecc7e47482a5f6dd620adf3d7afaea3
Michael Michlin <mmulya@outlook.com>
2020-08-03 00:11:04 -0400

    Create require.config() and require() code based on the extra libs specified.

--- mimcss/demo/examples/basic-template.tsx
+++ mimcss/demo/examples/basic-template.tsx
@@ -7,35 +7,17 @@ class MyStyles extends css.StyleDefinition
 {
 }
 
-
-
+// activate our styles
+let styles = css.activate( MyStyles);
 
 // Define component
 class MyComponent extends mim.Component
 {
-    // activated styles
-    private styles: MyStyles;
-
-    public willMount()
-    {
-        // activate our styles
-        this.styles = css.activate( MyStyles);
-    }
-
-    public willUnmount()
-    {
-        // deactivate our styles
-        css.deactivate( this.styles);
-    }
-
 	public render()
 	{
-        // render component HTML content
 		return <div/>
 	}
 }
-
-
 
 // Mount our component under the body element.
 mim.mount( new MyComponent());
